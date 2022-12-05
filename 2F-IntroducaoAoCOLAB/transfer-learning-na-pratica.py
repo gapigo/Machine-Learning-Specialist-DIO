ab8e6cb7f3dff0d4806089f64cdde3ee3c00c733
gapigo <x.gabriel12@gmail.com>
2022-12-04 23:04:44 -0300

    Adicionando rede de comparação de gatos e cachorros

--- 2F-IntroducaoAoCOLAB/transfer-learning-na-pratica.py
+++ 2F-IntroducaoAoCOLAB/transfer-learning-na-pratica.py
@@ -1,3 +1,5 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 import os
 import requests
 import zipfile
@@ -6,15 +8,22 @@ import tensorflow as tf
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from shutil import copyfile
+from threading import Thread
+from time import sleep
 
 def download(url: str, dest_folder: str):
     if not os.path.exists(dest_folder):
         os.makedirs(dest_folder)  # create folder if it does not exist
 
-    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
-    file_path = os.path.join(dest_folder, filename)
+    filename = dest_folder.split('/')[-1].replace(" ", "_")  # be careful with file names
+    #file_path = os.path.join(dest_folder, filename)
+    file_path = dest_folder
 
     r = requests.get(url, stream=True)
+    print('oi')
+    print(r.ok)
+    print(filename)
+    print(file_path)
     if r.ok:
         print("saving to", os.path.abspath(file_path))
         with open(file_path, 'wb') as f:
@@ -26,10 +35,133 @@ def download(url: str, dest_folder: str):
     else:  # HTTP status code 4XX/5XX
         print("Download failed: status code {}\n{}".format(r.status_code, r.text))
 
-local_zip = './tmp/cats-and-dogs.zip'
-if (not os.path.exists(local_zip)):
-	download('https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip', local_zip)
-zip_ref = zipfile.ZipFile(local_zip, 'r')
-zip_ref.extractall('/tmp')
-zip_ref.close()
 
+def show_dots(args):
+    global running
+    while running:
+        print(".", end="")
+        sleep(1)
+
+
+
+running = True
+print("a1")
+t = Thread(target = show_dots, args=[1])
+t.start()
+print("Alooo")
+if (not os.path.exists('./tmp/PetImages/Dog/0.jpg') or not os.path.exists('./tmp/PetImages/Cat/0.jpg')):
+    local_zip = './tmp/cats-and-dogs.zip'
+    #if (not os.path.exists(local_zip)):
+    #	download('https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip', local_zip)
+    zip_ref = zipfile.ZipFile(local_zip, 'r')
+    zip_ref.extractall('./tmp')
+    zip_ref.close()
+
+print(len(os.listdir('./tmp/PetImages/Cat/')))
+print(len(os.listdir('./tmp/PetImages/Cat/')))
+# Expected:
+# 12501
+# 12501
+
+try:
+    os.mkdir('./tmp/cats-v-dogs')
+    os.mkdir('./tmp/cats-v-dogs/training')
+    os.mkdir('./tmp/cats-v-dogs/testing')
+    os.mkdir('./tmp/cats-v-dogs/training/cats')
+    os.mkdir('./tmp/cats-v-dogs/training/dogs')
+    os.mkdir('./tmp/cats-v-dogs/testing/cats')
+    os.mkdir('./tmp/cats-v-dogs/testing/dogs')
+except OSError:
+    pass
+
+def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+    files = []
+    for filename in os.listdir(SOURCE):
+        file = SOURCE + filename
+        if os.path.getsize(file) > 0:
+            files.append(filename)
+        else:
+            print(filename + " is zero length, do ignoring.")
+
+    training_length = int(len(files) * SPLIT_SIZE)
+    testing_length = int(len(files) - training_length)
+    shuffled_set = random.sample(files, len(files))
+    training_set = shuffled_set[0:training_length]
+    testing_set = shuffled_set[-testing_length:]
+
+    for filename in training_set:
+        this_file = SOURCE + filename
+        destination = TRAINING + filename
+        copyfile(this_file, destination)
+
+    for filename in testing_set:
+        this_file = SOURCE + filename
+        destination = TESTING + filename
+        copyfile(this_file, destination)
+
+CAT_SOURCE_DIR = './tmp/PetImages/Cat/'
+TRAINING_CATS_DIR = './tmp/cats-v-dogs/training/cats/'
+TESTING_CATS_DIR = './tmp/cats-v-dogs/testing/cats/'
+DOG_SOURCE_DIR = './tmp/PetImages/Dog/'
+TRAINING_DOGS_DIR = './tmp/cats-v-dogs/training/dogs/'
+TESTING_DOGS_DIR = './tmp/cats-v-dogs/testing/dogs/'
+
+split_size = .9
+split_data(CAT_SOURCE_DIR, TRAINING_CATS_DIR, TESTING_CATS_DIR, split_size)
+split_data(DOG_SOURCE_DIR, TRAINING_DOGS_DIR, TESTING_DOGS_DIR, split_size)
+
+# Expected output
+# 666.jpg is zero length, so ignoring# 11702.jpg is zero length, so ignoring
+
+print(len(os.listdir('./tmp/cats-v-dogs/training/cats/')))
+print(len(os.listdir('./tmp/cats-v-dogs/training/dogs/')))
+print(len(os.listdir('./tmp/cats-v-dogs/testing/cats/')))
+print(len(os.listdir('./tmp/cats-v-dogs/testing/dogs/')))
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['acc'])
+
+TRAINING_DIR = './tmp/cats-v-dogs/training'
+train_datagen = ImageDataGenerator(rescale=1.0/255.)
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR, batch_size=250, class_mode='binary', target_size=(150, 150))
+
+VALIDATION_DIR = './tmp/cats-v-dogs/testing'
+validation_datagen = ImageDataGenerator(rescale=1.0/255.)
+validation_generator = train_datagen.flow_from_directory(VALIDATION_DIR, batch_size=250, class_mode='binary', target_size=(150, 150))
+
+# Expected Output
+# Found 22498 images belonging to 2 classes.
+# Found 2500 images belonging to 2 classes.
+
+# Note that this may take some time.
+history = model.fit(train_generator, epochs=15, steps_per_epoch=90, validation_data=validation_generator, validation_steps=6)
+
+print("Fim do programa")
+running = False
+
+# Retrieve a list of list results on training and test data sets for each training epoch
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc))  # Get number of epochs
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc, 'r', 'Training Accuracy')
+plt.plot(epochs, val_acc, 'b', 'Validation Accuracy')
+plt.title('Training and validation accuary')
+plt.figure()
+plt.show()
